94d596a554f929264aac8d9028f7bf88b9e192d2
Emile Nijssen <emile@emilenijssen.nl>
2020-12-21 17:10:45 +0100

    wip

--- drivers/pms/driver.js
+++ drivers/pms/driver.js
@@ -4,4 +4,32 @@ const PlexDriver = require('../../lib/PlexDriver');
 
 module.exports = class PlexMediaServerDriver extends PlexDriver {
 
+  async onInit() {
+    this.homey.flow.getActionCard('rescan')
+      .registerRunListener(async ({ device, library }) => {
+        const { key } = library;
+        return device.rescanLibrary({ key });
+      })
+      .registerArgumentAutocompleteListener('library', async (query, { device }) => {
+        const libraries = await device.getLibrarySections();
+        return libraries.map(library => ({
+          name: library.title,
+          key: library.key,
+        }));
+      });
+
+    this.homey.flow.getActionCard('refresh')
+      .registerRunListener(async ({ device, library }) => {
+        const { key } = library;
+        return device.refreshLibrary({ key });
+      })
+      .registerArgumentAutocompleteListener('library', async (query, { device }) => {
+        const libraries = await device.getLibrarySections();
+        return libraries.map(library => ({
+          name: library.title,
+          key: library.key,
+        }));
+      });
+  }
+
 };
